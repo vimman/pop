9c2e35fc5b040d342810e0bcba34e2330d82c069
LabElec <labelec@mac-labelec-06.42.fr>
2018-06-08 15:37:25 +0200

    Update datasheets, POP.tex, PPS.md

---
+++ Docs/PPS.md
@@ -0,0 +1,11 @@
+Purpose | Type | Register    | Value | Pin 
+----- | -------|-------------|-------|-----
+SPI|Input|SDI1R|0001|RPB5
+ |Output|RPB6R|0011|SDO1
+ UART|Input|U2RXR|0010|RPB1
+ |Output|RPB0-R|0010|U2TX
+Button|Input|INT1R|0100|RPB9
+ |Input|INT3R|0100|RPB8
+SD_SW|Input|INT4R|0011|RPB15|
+Buzzer|Input|RPB3R|0101|OC1|
+
---
+++ TP/firmware/2/type.h
@@ -0,0 +1,26 @@
+#ifndef TYPE_H
+#define	TYPE_H
+
+typedef char s8;
+typedef short s16;
+typedef long s32;
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned long u32;
+
+#define OUTPUT	0
+#define INPUT	1
+
+#define HIGH	1
+#define LOW		0
+
+#define true	1
+#define false	0
+
+#define BLINK	0
+#define DIM		1
+
+#define RISING_EDGE		1
+#define FALLING_EDGE	0
+
+#endif	/* TYPE_H */
